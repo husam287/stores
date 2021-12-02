68e9bf98e79602d296e0539b15ba622bdb5d05d6
Hossam Sherif <husam287@hotmail.com>
2021-12-02 17:52:19 +0200

    adjust signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent, pathMatch:'full'},
-  {path:'login', component:LoginComponent}
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent }
 ];
 
 @NgModule({
--- src/app/components/item-card/item-card.component.ts
+++ src/app/components/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent implements OnInit {
+
+  @Input('title') title: string = "";
+  @Input('description') description: string = "";
+  @Input('owner') owner: string = "";
+  @Input('imageUrl') imageUrl: string = "";
 
   constructor() { }
 
